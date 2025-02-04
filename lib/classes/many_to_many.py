ed579066c06151300926a324842d88256cfbbb6b
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-02-04 13:47:03 -0800

    First Commit 19F-13P

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,14 +1,58 @@
 class Article:
+
+    all = []
+
     def __init__(self, author, magazine, title):
-        self.author = author
+        self._author = author
         self.magazine = magazine
-        self.title = title
-        
+        self._title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        if isinstance(self._title, str) and 4 < len(self._title) < 51:
+            return self._title
+        else:
+            return "Title is invalid"
+
+    @property
+    def author(self):
+        if isinstance(self._author, Author):
+            return self._author
+        else:
+            return "Author is invalid"
+    
+    @author.setter
+    def author(self, new_value):
+        self._author = new_value
+
+    @title.setter
+    def title(self, new_value):
+        pass
+
+   
+    
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise Exception("Name must be a non-empty string")
+        self._name = name 
+        self.articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_value):
+        pass
 
     def articles(self):
+        if isinstance(self.articles, Article):
+            return self.articles
+        else:
+            return "No articles found"
         pass
 
     def magazines(self):
