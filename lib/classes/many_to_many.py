ea4cdc50cf77309f9977e53c90c4f5256f69a4a0
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-02-04 20:51:40 -0800

    5F 27P

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -7,6 +7,7 @@ class Article:
         self.magazine = magazine
         self._title = title
         Article.all.append(self)
+        author._articles.append(self)
 
     @property
     def title(self):
@@ -34,49 +35,64 @@ class Article:
     
 
 class Author:
+    all = []
+
     def __init__(self, name):
         if not isinstance(name, str) or len(name) == 0:
             raise Exception("Name must be a non-empty string")
         self._name = name 
-        self.articles = []
-
+        self._articles = []
+        Author.all.append(self)
+        
     @property
     def name(self):
         return self._name
-
+    
     @name.setter
     def name(self, new_value):
         pass
 
     def articles(self):
-        if isinstance(self.articles, Article):
-            return self.articles
-        else:
-            return "No articles found"
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        unique_magazines = []
+        for article in self._articles:
+            if article.magazine not in unique_magazines:
+                unique_magazines.append(article.magazine)
+        return unique_magazines
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        categories = {article.magazine.category for article in self._articles}
+        return list(categories) if categories else None
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        pass
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+        counts = {}
+        for article in self.articles():
+            author = article.author
+            if author in counts:
+                counts[author] += 1
+            else:
+                counts[author] = 1
+        authors = [author for author, count in counts.items() if count > 2]
+        return authors if authors else None
